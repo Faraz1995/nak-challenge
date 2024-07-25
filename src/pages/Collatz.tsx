ca61e368a19684b53dd2ce8ab5aab90ef751a6f3
FarazMehraien95 <farazmehraien95@gmail.com>
2024-07-26 00:27:06 +0330

    feat: complete ui of algorithm pages

--- src/pages/Collatz.tsx
+++ src/pages/Collatz.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import styles from './algorithm.module.css'
+import LargeInput from '../components/LargeInput'
+import Button from '../components/Button'
+import Card from '../components/Card'
 const Collatz = () => {
-  return <div>Collatz</div>
+  const [input, setInput] = useState('')
+
+  const calculateCollatz = () => {
+    console.log('hi')
+  }
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>Fibonacci</p>
+      <div className={styles.inputContainer}>
+        <LargeInput
+          label='enter your number'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <div className={styles.btnContainer}>
+        <Button text='submit' onClick={calculateCollatz} />
+      </div>
+      <div className={styles.divider} />
+
+      <div className={styles.resultContainer}>
+        <p className={styles.resultTitle}>Result</p>
+        <div className={styles.numbersContainer}>
+          <Card />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Collatz
--- src/pages/Fibonacci.tsx
+++ src/pages/Fibonacci.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import styles from './algorithm.module.css'
+import LargeInput from '../components/LargeInput'
+import Button from '../components/Button'
+import Card from '../components/Card'
 const Fibonacci = () => {
-  return <div>Fibonacci</div>
+  const [input, setInput] = useState('')
+
+  const calculateFibonacci = () => {
+    console.log('hi')
+  }
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>Fibonacci</p>
+      <div className={styles.inputContainer}>
+        <LargeInput
+          label='enter your number'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <div className={styles.btnContainer}>
+        <Button text='submit' onClick={calculateFibonacci} />
+      </div>
+      <div className={styles.divider} />
+
+      <div className={styles.resultContainer}>
+        <p className={styles.resultTitle}>Result</p>
+        <div className={styles.numbersContainer}>
+          <Card />
+          <Card text={input} />
+          <Card />
+        </div>
+      </div>
+      <p className={styles.number}>Your Number</p>
+    </div>
+  )
 }
 
 export default Fibonacci
