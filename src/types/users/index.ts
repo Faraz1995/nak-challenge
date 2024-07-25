0b123c40c6690673a5a572491620ccb776ce9b47
FarazMehraien95 <farazmehraien95@gmail.com>
2024-07-25 23:04:27 +0330

    modify type

--- src/types/users/index.ts
+++ src/types/users/index.ts
@@ -1,8 +1,10 @@
+type status = 'Active' | 'Not Active' | null | undefined
+
 export interface User {
   id: number
   name: string
   username: string
   email: string
   phone: string
-  status?: string
+  status?: status
 }
