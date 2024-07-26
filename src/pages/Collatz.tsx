ceecaec1c1ed00c6685aa36df9aa1708274ae20d
FarazMehraien95 <farazmehraien95@gmail.com>
2024-07-26 11:39:39 +0330

    feat: complete collatz page

--- src/pages/Collatz.tsx
+++ src/pages/Collatz.tsx
@@ -1,22 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './algorithm.module.css'
 import LargeInput from '../components/LargeInput'
 import Button from '../components/Button'
 import Card from '../components/Card'
 const Collatz = () => {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState<string>('')
+  const [result, setResult] = useState<string>('')
+
+  useEffect(() => {
+    setResult('')
+  }, [input])
+
+  const collatzSteps = (n: number): number => {
+    let steps = 0
+    while (n !== 1) {
+      if (n % 2 === 0) {
+        n = n / 2
+      } else {
+        n = 3 * n + 1
+      }
+      steps++
+    }
+    return steps
+  }
 
   const calculateCollatz = () => {
-    console.log('hi')
+    const steps = collatzSteps(parseInt(input))
+    setResult(steps.toString())
   }
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      calculateCollatz()
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <p className={styles.title}>Fibonacci</p>
+      <p className={styles.title}>Collatz Conjecture</p>
       <div className={styles.inputContainer}>
         <LargeInput
           label='enter your number'
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyPress={handleKeyPress}
         />
       </div>
       <div className={styles.btnContainer}>
@@ -27,7 +54,7 @@ const Collatz = () => {
       <div className={styles.resultContainer}>
         <p className={styles.resultTitle}>Result</p>
         <div className={styles.numbersContainer}>
-          <Card />
+          <Card text={result} />
         </div>
       </div>
     </div>
